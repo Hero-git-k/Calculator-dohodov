cc6a821010ee2354c3c0c9cc36a0047ebe211a9f
Denis <82368652+Hero-git-k@users.noreply.github.com>
2021-12-17 02:08:52 +0300

    Add files via upload

---
+++ 19.10/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19._10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            int b;
+            int c;
+            int s;
+            int d;
+            a = Convert.ToInt32(textBox3.Text); 
+            b = Convert.ToInt32(textBox2.Text);
+            c = Convert.ToInt32(textBox1.Text);
+            d = (c/100);
+            s = ((a*(1+d/12))^b);
+            textBox4.Text = Convert.ToString(s);
+        }
+    }
+}
